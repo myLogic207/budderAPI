de20a4c11724e5339b50d0b02a75118bd32479ca
myLogic207 <lijon.fogel@gmail.com>
2022-03-24 19:02:38 +0100

    rawBudderDATAv0.0.1/Based

--- app/core.js
+++ app/core.js
@@ -4,7 +4,10 @@ const app = express();
 const port = parseInt(process.env.APP_PORT, 10);
 const host = process.env.APP_HOST;
 
-
+if(process.env.DATABSE){
+    const data = require('./data/databases');
+    const db = data.useDB();
+}
 
 const backend = require("./backend/server");
 const frontend = require("./frontend/client");
