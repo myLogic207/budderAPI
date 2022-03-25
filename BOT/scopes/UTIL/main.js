2e1b8f5ea51498c1479187dd41bb2a62d2c95c35
Lijon Fogel <lijon.fogel@gmail.com>
2022-03-25 23:16:55 +0100

    eLog now working correctly when in dev env

--- BOT/scopes/UTIL/main.js
+++ BOT/scopes/UTIL/main.js
@@ -16,9 +16,17 @@ module.exports = {
     eLog : function(msg) {
         if (process.env.ENV === 'dev') {
             console.log(msg);
-        } else if (process.env.ELOG_ENABLED && process.env.DB_ENABLED) {
+        }
+        if (process.env.ELOG_ENABLED && process.env.DB_ENABLED) {
             const db = require('../DATABASE/actions');
             db.logMessage(msg);
+        } else if (process.env.ELOG_ENABLED) {
+            console.warn("[UTIL] eLog is enabled but budderDATA is not");
+            console.warn("[UTIL] To use eLog a database is required");
+            console.warn("[UTIL] Please disable eLog or enable budderDATA");
+            console.log(msg);
+        } else {
+            console.log(msg);
         }
     }
 }
