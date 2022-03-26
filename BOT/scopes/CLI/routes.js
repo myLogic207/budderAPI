c8c73308ed604c8ab82ba12199ee8a10d803924b
Lijon Fogel <lijon.fogel@gmail.com>
2022-03-26 01:58:18 +0100

    split routes into own files for dynamic loading

--- BOT/scopes/CLI/routes.js
+++ BOT/scopes/CLI/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const path = require('path');
-const { discordHandle } = require("./scopes/discord");
+const { coreHandle } = require("./scopes/core");
 
 router.use(express.static(path.join(__dirname, "frontend")));
 
@@ -10,11 +10,7 @@ router.get('/', function(req, res){
 });
 
 router.get('/command/core', function(req, res){
-  res.send("CORE command");
-});
-
-router.get('/command/discord', async (req, res) => {
-  res.send(await discordHandle(req.query.cmd));
+  res.send(await coreHandle(req.query.cmd));
 });
 
 module.exports = router;
