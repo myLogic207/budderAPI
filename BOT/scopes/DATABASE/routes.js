ee9cd954bde7eac68ddf3f274046029b3a30fee0
Lijon Fogel <lijon.fogel@gmail.com>
2022-04-04 00:00:40 +0200

    fix

--- BOT/scopes/DATABASE/routes.js
+++ BOT/scopes/DATABASE/routes.js
@@ -1,5 +1,4 @@
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
 const path = require('path');
 const eLogPath = require("../../config.json").eLog.eLogPath;
 const { eLog } = require(eLogPath);
