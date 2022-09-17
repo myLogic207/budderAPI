a56d156a61bb966cb5bdb3d407a03438c897b585
myLogic207 <lijon.fogel@gmail.com>
2022-09-18 01:26:35 +0200

    Fixing smaller issues

--- BOT/scopes/SCANNER/main.js
+++ BOT/scopes/SCANNER/main.js
@@ -1,17 +1,17 @@
 "use strict";
 const { logLevel, eLog } = require(process.env.UTILS);
 
-const Scanner = new Map();
+const Scanners = new Map();
 const States = new Map();
 
 const State = {
-    TODO,
-    TODEL,
-    INPROG,
-    DONE,
-    SKIP,
-    ERROR,
-    OFF
+    TODO: "TODO",
+    TODEL: "TODEL",
+    INPROG: "INPROG",
+    DONE: "DONE",
+    SKIP: "SKIP",
+    ERROR: "ERROR",
+    OFF: "OFF"
 }
 
 const Marker = [
@@ -25,16 +25,30 @@ const Marker = [
 ];
 
 module.exports = {
-    start: () => {
-        eLog(logLevel.INFO, "SCANNER", "Starting Hotdeploy Scanners");
-        const DeploymentScanner = require("./deployments");
-        Scanner.set(DeploymentScanner.scannerID, DeploymentScanner.name);
-        DeploymentScanner.start();
-        eLog(logLevel.STATUS, "SCANNER", "Deployment scanner started");
-        const Handler = require("./handler");
-        Scanner.set(Handler.scannerID, Handler.name);
-        Handler.start();
-        eLog(logLevel.STATUS, "SCANNER", "Deployment handler started");
+    start: async () => {
+        return new Promise((resolve, reject) => {
+            eLog(logLevel.INFO, "SCANNER", "Starting Hotdeploy Scanners");
+            const DeploymentScanner = require(`${__dirname}${process.env.SEP}listeners${process.env.SEP}deployments`);
+            const ds = new DeploymentScanner();
+            eLog(logLevel.DEBUG, "SCANNER", `Scanner ${ds.name} registered with ID ${ds.scannerID}`);
+            //ds.start();
+            eLog(logLevel.STATUS, "SCANNER", "Deployment scanner started");
+            const Handler = require(`${__dirname}${process.env.SEP}listeners${process.env.SEP}handler`);
+            const ms = new Handler();
+            ms.start();
+            eLog(logLevel.STATUS, "SCANNER", "Deployment handler started");
+            resolve();
+        });
+    },
+    end: async () => {
+        return new Promise((resolve, reject) => {
+            eLog(logLevel.INFO, "SCANNER", "Stopping Hotdeploy Scanners");
+            Scanners.forEach(scanner => {
+                scanner.stop();
+            });
+            eLog(logLevel.STATUS, "SCANNER", "Hotdeploy Scanners stopped");
+            resolve();
+        });
     },
     register: (scanner) => {
         eLog(logLevel.INFO, "SCANNER", "Registered Scanner: " + scanner.name);
@@ -49,15 +63,15 @@ module.exports = {
     State: State,
     Marker: Marker,
     getState: (file = null) => {
-        if(file) return States.has(file) ? States.get(file) : null;
+        if(file) return States.has(file.name) ? States.get(file.name) : null;
         else return States;
     },
     setState: (file, state) => {
-        States.set(file, state);
+        States.set(file.name, state);
     },
     isMarkerFile(file){
         // return file.includes('deploy');
-        eLog(logLevel.DEBUG, `SCANNER-${this.name}`, `Checking if ${file} is a marker file`);
-        return Markers.includes(file.split('.').pop());
+        eLog(logLevel.DEBUG, `SCANNER-MAIN`, `Checking if ${file.name} is a marker file`);
+        return Marker.includes(file.name.split('.').pop());
     }
 }
