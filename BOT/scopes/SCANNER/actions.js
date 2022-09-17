98ccc0be134cf2e745568baef7c29b55018840bb
myLogic207 <lijon.fogel@gmail.com>
2022-09-18 01:26:11 +0200

    Add init and shutdown for Deployer

--- BOT/scopes/SCANNER/actions.js
+++ BOT/scopes/SCANNER/actions.js
@@ -1,12 +1,33 @@
 "use strict";
-const { start, getScannerByUUID, getScannerByName, register } = require("./main");
-const Scanner = require("./scanner");
+const { eLog, logLevel } = require(process.env.UTILS);
+const { start, end, getScannerByUUID, getScannerByName, register } = require(`${__dirname}${process.env.SEP}main`);
+const Scanner = require(`${__dirname}${process.env.SEP}listeners${process.env.SEP}scanner`);
 
-const Scanners = new Map();
+
 module.exports = {
-    init: () => {
-        eLog(logLevel.STATUS, "SCANNER", "Initializing SCANNER");
-        start();
+    init: async () => {
+        return new Promise((resolve, reject) => {
+            eLog(logLevel.STATUS, "SCANNER", "Initializing SCANNER");
+            start().then(() => {
+                eLog(logLevel.STATUS, "SCANNER", "SCANNER fully initialized");
+                resolve();
+            }).catch(err => {
+                eLog(logLevel.WARN, "SCANNER", "SCANNER initialization failed");
+                reject(err);
+            });
+        });
+    },
+    shutdown: async () => {
+        return new Promise((resolve, reject) => {
+            eLog(logLevel.STATUS, "SCANNER", "Shutting down SCANNER");
+            end().then(() => {
+                eLog(logLevel.STATUS, "SCANNER", "SCANNER fully shutdown");
+                resolve();
+            }).catch(err => {
+                eLog(logLevel.ERROR, "SCANNER", "SCANNER shutdown failed");
+                reject(err);
+            });
+        });
     },
     newScanner: (scannername, scannerdir, scannerinterval) => {
         eLog(logLevel.WARN, "SCANNER", "Initializing new custom scanner");
