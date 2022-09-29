d8ace23f8614f11433f43e3e4c2a9555e048b3f3
myLogic207 <lijon.fogel@gmail.com>
2022-09-29 23:55:12 +0200

    Fix loading order

--- BOT/scopes/CORE/core.js
+++ BOT/scopes/CORE/core.js
@@ -5,27 +5,24 @@ process.env.SEP = platform === "win32" ? "\\" : "/";
 
 function Logo(){
     return `
- _               _     _            _    ____ ___ 
-| |__  _   _  __| | __| | ___ _ __ / \\  |  _ \\_ _|
-| '_ \\| | | |/ _\` |/ _\` |/ _ \\ '__/ _ \\ | |_) | | 
-| |_) | |_| | (_| | (_| |  __/ | / ___ \\|  __/| | 
-|_.__/ \\__,_|\\__,_|\\__,_|\\___|_|/_/   \\_\\_|  |___|
-
-`;
+     _               _     _            _    ____ ___ 
+    | |__  _   _  __| | __| | ___ _ __ / \\  |  _ \\_ _|
+    | '_ \\| | | |/ _\` |/ _\` |/ _ \\ '__/ _ \\ | |_) | | 
+    | |_) | |_| | (_| | (_| |  __/ | / ___ \\|  __/| | 
+    |_.__/ \\__,_|\\__,_|\\__,_|\\___|_|/_/   \\_\\_|  |___|
+    
+    `;
 }
-
 
 async function main(){
     const startTime = new Date();
-    process.env.LOG = `${__dirname}${process.env.SEP}bin${process.env.SEP}logger`;
-    log(logLevel.STATUS, "CORE", "Loading Config");
-    const { CONFIG } = require("./bin/config");
-
-    // init eLog - Is ugly, will be cleaned with requirements
-    const logInit = await require(`${process.cwd()}${process.env.SEP}modules${process.env.SEP}logger${process.env.SEP}actions`).init();
-    CONFIG().modules.push(logInit[0])
-    process.env.LOG = logInit[1];
-    delete require.cache[process.env.LOG];
+    process.env.LOG = `${__dirname}${process.env.SEP}bin${process.env.SEP}logger.js`;
+    
+    // log(logLevel.STATUS, "CORE", "Loading Config");
+    require("./bin/config").initConfig();
+    // const { CONFIG } = require("./bin/config");
+    
+    require("./bin/logger").initLogger();
     const { log, logLevel } = require(process.env.LOG);
     
     log(logLevel.STATUS, "CORE", `Starting BOT at ${startTime}`);    
