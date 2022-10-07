9c7c61eef26e72bfc11e1bc7f1333c627e21d581
myLogic207 <lijon.fogel@gmail.com>
2022-10-07 22:42:18 +0200

    Remove DB (will be refactored into module at feat/db_module)

--- BOT/scopes/DATABASE/croutes/cli.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { dataHandle } = require("../clicmds");
-const router = express.Router();
-
-router.get('/command/data', async (req, res) => {
-    res.send(await dataHandle(req.query.cmd));
-  });
-
-module.exports = router;
